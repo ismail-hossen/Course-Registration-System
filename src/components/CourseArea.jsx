3aa4b6f041053bf26cc7fe1898c423636927b09d
ismail-hossen <satict2016@gmail.com>
2023-09-16 11:20:13 +0600

    Credit Hour Remaining Area design done

--- src/components/CourseArea.jsx
+++ src/components/CourseArea.jsx
@@ -1,4 +1,5 @@
 import CourseCard from "./CourseCard";
+import CourseName from "./CourseName";
 
 const CourseArea = () => {
   return (
@@ -6,13 +7,15 @@ const CourseArea = () => {
       <h1 className="text-center text-black text-4xl font-bold mt-6">
         Course Registration
       </h1>
-      <div className="grid grid-cols-3 w-8/12 gap-3 mt-4">
-        <CourseCard></CourseCard>
-        <CourseCard></CourseCard>
-        <CourseCard></CourseCard>
-        <CourseCard></CourseCard>
-        <CourseCard></CourseCard>
-        <CourseCard></CourseCard>
+      <div className="flex gap-3">
+        <div className="grid grid-cols-3 w-8/12 gap-3 mt-4">
+          <CourseCard></CourseCard>
+          <CourseCard></CourseCard>
+          <CourseCard></CourseCard>
+        </div>
+        <div>
+          <CourseName></CourseName>
+        </div>
       </div>
     </div>
   );
